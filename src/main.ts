d0a05c37089ef2c14007bc9c3c1d80794e09073d
georgesamoskey <georgeskatiera@gmail.com>
2024-03-23 16:04:00 +0100

    Le dernier commit

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('', app, document);
   SwaggerModule.setup('api-docs', app, document);
-  await app.listen(3000);
+  await app.listen(3011);
 }
 bootstrap();
